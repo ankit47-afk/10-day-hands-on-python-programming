96815e3b623cb328bf0fea7f2f99d62653220ce7
Ankit kumar <ankitk44164@gmail.com>
2025-11-28 18:42:51 +0530

    Add files via upload

---
+++ eamil sending resiving .py
@@ -0,0 +1,17 @@
+# how to send email through python .
+import smtplib # this lib is used to send and reciving emails 
+import ssl # ssl is used to make secure circuit 
+from email.message import EmailMessage # means 
+EMAIL = "m"
+APP_PASSWORD = ""
+RECEIVER = ""
+msg = EmailMessage()
+msg["From"] = EMAIL
+msg["To"] = RECEIVER
+msg["Subject"] = " hi my name is ankit "
+msg.set_content("this email is changed by pytho code ...")
+context =ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(EMAIL, APP_PASSWORD)
+    server.send_message(msg)
+    
